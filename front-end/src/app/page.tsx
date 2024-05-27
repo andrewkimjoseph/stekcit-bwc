7884af904ae940865c0a646046a487c690e7297e
andrewkimjoseph <andrewkimjoseph@gmail.com>
2024-05-27 13:43:28 +0300

    fixed page bug

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -21,6 +21,8 @@ export default function Home() {
         const doesUserExist = await checkIfUserExists(address);
         setUserExists(doesUserExist);
         setIsLoading(false);
+      } else {
+        setIsLoading(false);
       }
     };
     checkIfUserExistsAndSet();
