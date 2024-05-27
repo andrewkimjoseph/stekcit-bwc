51e72e0f44c91a92ee0f3e6e4a153bf5f11d5d3c
andrewkimjoseph <andrewkimjoseph@gmail.com>
2024-05-27 12:22:07 +0300

    updated layout

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -16,8 +16,6 @@ import { celo, celoAlfajores } from "wagmi/chains";
 import "./globals.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 const config = getDefaultConfig({
   appName: "Stekcit BwC",
@@ -37,20 +35,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const router = useRouter();
-
-  const userLoggedIn = true;
-
-  useEffect(() => {
-    if (userLoggedIn) {
-      router.push("/become-a-user");
-    }
-    else {
-      router.push("/");
-    }
-  }, [userLoggedIn]);
-
-
   return (
     <html lang="en" className={fonts.dmSans.variable}>
       <head>
